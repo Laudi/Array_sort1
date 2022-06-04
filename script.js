c1df8033b29f4bf06a4aeac96acee0acb5360631
Laudi <aleckoles@gmail.com>
2022-06-04 23:32:28 +0300

    First

---
+++ script.js
@@ -0,0 +1,31 @@
+let names = ['Hofstadter', 'Cooper', 'Wolowitz', 'Koothrappali'];
+let sorted = names.sort((a, b) => {
+    if (a > b) {
+        return -1;
+    }
+    if (a < b) {
+        return 1;
+    }
+
+    return 0;
+
+})
+
+console.log(sorted);
+
+// Здесь будет ваш код
+const sortByBirth = (arr) => {
+    return arr.sort((a, b) => {
+        aDob = a.split(', ')[1];
+        bDob = b.split(', ')[1];
+        return +aDob - +bDob;
+    })
+}
+
+
+
+console.log(sortByBirth([
+    'Nicola Tesla, 1856',
+    'Alan Turing, 1912',
+    'Ada Lovelace, 1815'
+])) // ['Ada Lovelace, 1815', 'Nicola Tesla, 1856', 'Alan Turing, 1912']
